2f0ce4c10bc8d130b05638c8fa4a488f6470d103
tomrutherford <tmrutherford@gmail.com>
2022-07-26 15:08:28 -0600

    Initial commit

---
+++ frame_factory/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for frame_factory."""
+
+__author__ = """Tom Rutherford"""
+__email__ = 'foreverwintr@gmail.com'
+__version__ = '0.1.0'
---
+++ frame_factory/tests/test_frame_factory.py
@@ -0,0 +1,9 @@
+
+from frame_factory.recipe import recipes
+
+def test_():
+
+    assert 0
+
+
+test_()
