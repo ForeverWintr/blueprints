119de62460c6c9a9ce6505f378f0b17694aedb42
tomrutherford <tmrutherford@gmail.com>
2022-07-30 12:41:46 -0700

    Add abstract method

--- frame_factory/recipes.py
+++ frame_factory/recipes.py
@@ -10,6 +10,15 @@ class Recipe(ABC):
     def identity_key(self) -> tuple:
         """Return a key from this recipe's parameters. This is used for hashing and equality, and it should be possible to re-create the recipe using Recipe(**dict(r.key()))"""
 
+    def get_dependency_recipes(self) -> tuple[Recipe, ...]:
+        """Return a tuple of recipes that this recipe depends on"""
+        return ()
+
+    @abstractmethod
+    def extract_from_dependency(self, *args) -> tp.Any:
+        """Given positional dependencies, extract the data that this recipe describes. args will be
+        the results of instantiating the recipes returned by `get_dependency_recipes` above"""
+
     def __hash__(self):
         return hash(self.identity_key())
 
@@ -17,7 +26,3 @@ class Recipe(ABC):
         if not isinstance(other, Recipe):
             return NotImplemented
         return self.identity_key() == other.identity_key()
-
-    def get_dependency_recipes(self) -> tuple[Recipe, ...]:
-        """Return a tuple of recipes that this recipe depends on"""
-        return ()
