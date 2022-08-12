8980454dd8404e55347ce84cd7f7859e677cde60
Tom Rutherford <tmrutherford@gmail.com>
2022-08-12 15:40:35 -0700

    WIP. simple visualization

--- assembler/blueprint.py
+++ assembler/blueprint.py
@@ -9,6 +9,56 @@ from assembler import util
 from assembler.constants import NodeAttrs, BuildStatus
 
 
+def get_blueprint_layout(
+    g: nx.DiGraph, vertical_increment: int = 2, horizontal_increment: int = 1
+) -> dict[Recipe, tuple[int, int]]:
+    bottom_layer = {n for n, d in g.out_degree() if d == 0}
+    x = 0
+    positions = {}
+
+    while bottom_layer:
+        next_layer = set()
+        y = 0
+        for node in bottom_layer:
+            positions[node] = (y, x)
+            y += vertical_increment
+            for p in g.predecessors(node):
+                next_layer.add(p)
+        bottom_layer = next_layer
+        x += horizontal_increment
+    return positions
+
+
+def make_dependency_graph(recipes: tp.Iterable[Recipe]) -> nx.DiGraph:
+    g = nx.DiGraph()
+    outputs = set(recipes)
+    to_process = list(outputs)
+    processed = set()
+    while to_process:
+        r = to_process.pop()
+        g.add_node(
+            r,
+            **{
+                NodeAttrs.output: r in outputs,
+                NodeAttrs.build_status: BuildStatus.NOT_STARTED,
+            },
+        )
+        for d in r.get_dependency_recipes():
+            g.add_edge(d, r)
+
+            if d not in processed:
+                to_process.append(d)
+
+        processed.add(r)
+
+    if nx.dag.has_cycle(g):
+        cycles = nx.find_cycle(g)
+        raise exceptions.ConfigurationError(
+            f"The given recipe produced dependency cycles: {cycles}"
+        )
+    return g
+
+
 class Blueprint:
     def __init__(self, dependency_graph: nx.DiGraph):
         """A Blueprint describes how to construct recipes and their depenencies.
@@ -22,30 +72,10 @@ class Blueprint:
     @classmethod
     def from_recipes(cls, recipes: tp.Iterable[Recipe]) -> Blueprint:
         """Create a blueprint from the given recipe."""
-        g = nx.DiGraph()
-        outputs = set(recipes)
-        to_process = list(outputs)
-        processed = set()
-        while to_process:
-            r = to_process.pop()
-            g.add_node(
-                r,
-                **{
-                    NodeAttrs.output: r in outputs,
-                    NodeAttrs.build_status: BuildStatus.NOT_STARTED,
-                },
-            )
-            for d in r.get_dependency_recipes():
-                g.add_edge(d, r)
-
-                if d not in processed:
-                    to_process.append(d)
-
-            processed.add(r)
-
-        if nx.dag.has_cycle(g):
-            cycles = nx.find_cycle(g)
-            raise exceptions.ConfigurationError(
-                f"The given recipe produced dependency cycles: {cycles}"
-            )
+        g = make_dependency_graph(recipes)
         return cls(g)
+
+    def draw(self, ax: plt.Axes) -> None:
+        """Draw the blueprint on the given matplotlib ax object"""
+        positions = get_blueprint_layout(self._dependency_graph)
+        nx.draw(self._dependency_graph, pos=positions, ax=ax)
--- assembler/tests/test_blueprint.py
+++ assembler/tests/test_blueprint.py
@@ -1,13 +1,34 @@
 from __future__ import annotations
+import typing as tp
 
 import pytest
 
+
 from assembler.recipes import Recipe
 from assembler.factory import Factory, FrameFactoryMP
-from assembler.blueprint import Blueprint
+from assembler.blueprint import Blueprint, get_blueprint_layout, make_dependency_graph
 from assembler.constants import NodeAttrs, BuildStatus
 from assembler import exceptions
 from assembler.tests.conftest import TestData, TestColumn, TABLES, MultiColumn
+
+
+class Node(Recipe):
+    """A simple recipe used for making graphs"""
+
+    name: str
+    dependencies: tuple[Node]
+
+    def get_dependency_recipes(self) -> tuple[Node]:
+        return self.dependencies
+
+    def extract_from_dependency(self, *args) -> tp.Any:
+        pass
+
+    def __repr__(self):
+        return f"{(type(self).__name__)}({self.name!r}, {self.dependencies})"
+
+    def __str__(self):
+        return self.name
 
 
 def test_from_recipes():
@@ -52,9 +73,39 @@ def test_from_recipes_no_cycles() -> None:
             pass
 
         def __repr__(self):
-            return f"{(type(self).__name__)}({self.name}, {self.target})"
+            return f"{(type(self).__name__)}({self.name!r}, {self.target})"
 
     with pytest.raises(exceptions.ConfigurationError) as e:
         Blueprint.from_recipes([Bad(name="a", target="b")])
 
         assert e.match("The given recipe produced dependency cycles")
+
+
+def test_get_blueprint_layout():
+    a = Node(name="a", dependencies=())
+    d = Node(name="d", dependencies=())
+    b = Node(name="b", dependencies=(a,))
+    c = Node(name="c", dependencies=(a, d))
+
+    g = make_dependency_graph([b, c])
+    layout = get_blueprint_layout(g)
+    assert layout == {b: (0, 0), c: (1, 0), a: (0, 1), d: (1, 1)}
+
+
+def test_visualize():
+    # Slow import.
+    from matplotlib import pyplot as plt
+
+    a = Node(name="a", dependencies=())
+    d = Node(name="d", dependencies=())
+    b = Node(name="b", dependencies=(a,))
+    c = Node(name="c", dependencies=(a, d))
+
+    b = Blueprint.from_recipes([b, c])
+
+    fig = plt.Figure()
+    ax = fig.subplots()
+
+    b.draw(ax)
+    fig.savefig("/tmp/tmp.png")
+    raise NotImplementedError("WIP")  # TODO REMOVE
