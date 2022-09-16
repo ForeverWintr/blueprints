aadc5a3d618e8ceb963b944a3a09f43bd516b970
Tom Rutherford <tmrutherford@gmail.com>
2022-09-16 15:23:07 -0700

    RM setup

--- assembler/__init__.py
+++ assembler/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for assembler."""
 
 __author__ = """Tom Rutherford"""
-__email__ = 'foreverwintr@gmail.com'
-__version__ = '0.1.0'
+__email__ = "foreverwintr@gmail.com"
+__version__ = "0.1.0"
