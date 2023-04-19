9d76b5bfa6a53fbaa43f2940ee6042dbaf67cc8e
Tom Rutherford <tmrutherford@gmail.com>
2023-04-18 18:11:51 -0700

    Add cytoscape experiment

---
+++ assembler/cytoscape_experiment.py
@@ -0,0 +1,34 @@
+from dash import Dash, html
+import dash_cytoscape as cyto
+
+cyto.load_extra_layouts()
+app = Dash(__name__)
+
+cytoscape = cyto.Cytoscape(
+    id="cytoscape-two-nodes",
+    layout={"name": "dagre"},
+    style={"width": "90vw", "height": "90vh"},
+    elements=[
+        {
+            "data": {"id": "one", "label": "Node 1"},
+            # "position": {"x": 75, "y": 75},
+            "grabbable": False,
+            "classes": "red triangle",
+        },
+        {
+            "data": {"id": "two", "label": "Node 2"},
+            # "position": {"x": 200, "y": 200},
+            "grabbable": False,
+        },
+        {"data": {"source": "one", "target": "two"}},
+    ],
+    stylesheet=[  # Group selectors
+        {"selector": "node", "style": {"content": "data(label)"}},
+        # Class selectors
+        {"selector": ".red", "style": {"background-color": "red", "line-color": "red"}},
+        {"selector": ".triangle", "style": {"shape": "triangle"}},
+    ],
+)
+app.layout = html.Div([cytoscape])
+if __name__ == "__main__":
+    app.run_server(debug=True)
