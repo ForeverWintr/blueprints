f9fd2776e9148df36ee373e653c229c116feb588
tomrutherford <tmrutherford@gmail.com>
2023-04-20 14:17:51 -0600

    Wip. Learning

--- assembler/cytoscape_experiment.py
+++ assembler/cytoscape_experiment.py
@@ -1,5 +1,8 @@
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output, State
 import dash_cytoscape as cyto
+from dash import ctx
+
 
 cyto.load_extra_layouts()
 app = Dash(__name__)
@@ -21,7 +24,14 @@ cytoscape = cyto.Cytoscape(
             "grabbable": False,
             "classes": "square",
         },
+        {
+            "data": {"id": "three", "label": "Node 3"},
+            # "position": {"x": 200, "y": 200},
+            "grabbable": False,
+            "classes": "square",
+        },
         {"data": {"source": "one", "target": "two"}},
+        {"data": {"source": "three", "target": "two"}},
     ],
     stylesheet=[  # Group selectors
         {"selector": "node", "style": {"content": "data(label)"}},
@@ -41,5 +51,28 @@ app.layout = html.Div(
         html.Button("Next", id="btn-next"),
     ]
 )
+
+
+@app.callback(
+    Output("cytoscape-elements-callbacks", "elements"),
+    Output("step-index", "data"),
+    Input("step-index", "data"),
+    Input("btn-prev", "n_clicks"),
+    Input("btn-next", "n_clicks"),
+)
+def update_graph_scatter(step_idx, btn_prev_clicks, btn_next_clicks):
+    btn_clicked = ctx.triggered_id
+
+    if btn_clicked == "btn-next":
+        step_idx = step_idx + 1
+    elif btn_clicked == "btn-prev":
+        step_idx = step_idx - 1
+
+    while step_idx > len(steps):
+        steps.append(make_step())
+
+    return steps[step_idx - 1], step_idx
+
+
 if __name__ == "__main__":
     app.run(debug=True)
