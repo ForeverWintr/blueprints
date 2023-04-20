a0017e1d9dddc8d360cb32c1257da74f94f66dc9
tomrutherford <tmrutherford@gmail.com>
2023-04-20 16:41:21 -0600

    WIP. Need separate process

---
+++ assembler/renderers/dash_renderer.py
@@ -0,0 +1,34 @@
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output, State
+import dash_cytoscape as cyto
+from dash import ctx
+
+
+class DashRenderer:
+    def __enter__(self):
+        cyto.load_extra_layouts()
+
+        graph = cyto.Cytoscape(
+            id="blueprint",
+            layout={"name": "dagre"},
+            style={"width": "100vw", "height": "90vh"},
+            elements=[],
+            stylesheet=[
+                {"selector": "node", "style": {"content": "data(label)"}},
+                {
+                    "selector": ".red",
+                    "style": {"background-color": "red", "line-color": "red"},
+                },
+                {"selector": ".square", "style": {"shape": "square"}},
+            ],
+        )
+        self.app = Dash(__name__)
+        self.app.layout = html.Div(
+            [
+                graph,
+                # The memory store reverts to the default on every page refresh
+                dcc.Store("step-index", data=1, storage_type="memory"),
+                html.Button("Previous", id="btn-prev"),
+                html.Button("Next", id="btn-next"),
+            ]
+        )
