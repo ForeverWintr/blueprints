4c5a9577f4cfd78ffba5f4c733f3ef1c83fd7139
tomrutherford <tmrutherford@gmail.com>
2023-04-20 13:36:34 -0600

    Add buttons

--- assembler/cytoscape_experiment.py
+++ assembler/cytoscape_experiment.py
@@ -1,4 +1,4 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import dash_cytoscape as cyto
 
 cyto.load_extra_layouts()
@@ -7,18 +7,19 @@ app = Dash(__name__)
 cytoscape = cyto.Cytoscape(
     id="cytoscape-two-nodes",
     layout={"name": "dagre"},
-    style={"width": "100vw", "height": "100vh"},
+    style={"width": "100vw", "height": "90vh"},
     elements=[
         {
             "data": {"id": "one", "label": "Node 1"},
             # "position": {"x": 75, "y": 75},
             "grabbable": False,
-            "classes": "red triangle",
+            "classes": "red square",
         },
         {
             "data": {"id": "two", "label": "Node 2"},
             # "position": {"x": 200, "y": 200},
             "grabbable": False,
+            "classes": "square",
         },
         {"data": {"source": "one", "target": "two"}},
     ],
@@ -26,9 +27,19 @@ cytoscape = cyto.Cytoscape(
         {"selector": "node", "style": {"content": "data(label)"}},
         # Class selectors
         {"selector": ".red", "style": {"background-color": "red", "line-color": "red"}},
-        {"selector": ".triangle", "style": {"shape": "triangle"}},
+        {"selector": ".square", "style": {"shape": "square"}},
     ],
 )
-app.layout = html.Div([cytoscape])
+
+
+app.layout = html.Div(
+    [
+        cytoscape,
+        # The memory store reverts to the default on every page refresh
+        dcc.Store("step-index", data=1, storage_type="memory"),
+        html.Button("Previous", id="btn-prev"),
+        html.Button("Next", id="btn-next"),
+    ]
+)
 if __name__ == "__main__":
     app.run(debug=True)
