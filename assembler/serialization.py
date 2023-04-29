cb61bd2da3f322eed38ae6998c355da4f28f4bf0
tomrutherford <tmrutherford@gmail.com>
2023-04-28 20:13:52 -0700

    WIP. Add RecipeRegistry

--- assembler/serialization.py
+++ assembler/serialization.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import typing as tp
 import json
+import dataclasses
 
 from frozendict import frozendict
 
@@ -8,6 +9,36 @@ from assembler import util
 
 if tp.TYPE_CHECKING:
     from assembler.recipes.base import Recipe, Dependencies, DependencyRequest
+
+
+class RecipeRegistry:
+    KEY_PREFIX = "RR"
+
+    def __init__(
+        self,
+        key_to_recipe: dict[str, Recipe] | None = None,
+    ):
+        """Maps recipes to process-unique ids. For use in serializing."""
+        self._recipe_to_key = {}
+        self._key_to_recipe = {}
+        if key_to_recipe:
+            self._key_to_recipe = key_to_recipe
+            self._recipe_to_key = {r: k for k, r in key_to_recipe.items()}
+
+    @classmethod
+    def from_recipes(cls, recipes: tp.Iterable[Recipe]) -> tp.Self:
+        return cls(
+            {
+                f"{cls.KEY_PREFIX}_{id(r)}": r
+                for r, _ in util.recipes_and_dependencies(recipes)
+            }
+        )
+
+    def add(self, recipe: Recipe) -> None:
+        if recipe not in self._recipe_to_key:
+            key = f"{self.KEY_PREFIX}_{id(recipe)}"
+            self._key_to_recipe[key] = recipe
+            self._recipe_to_key[recipe] = key
 
 
 def recipe_registry(recipes: tp.Iterable[Recipe]) -> dict[int, Recipe]:
@@ -43,4 +74,10 @@ class ImmutableJsonDecoder(json.JSONDecoder):
 
 
 def recipe_to_json(recipe: Recipe) -> str:
+    """Convert to a json representation that does not duplicate recipes. A recipe's
+    dependencies are replaced with IDs into a registry mapping."""
+
+    # registry that maps both ways
+    # for each recipe in registry, replace any recipe fields with references to registry.
+    r = RecipeRegistry.from_recipes([recipe])
     asdf
