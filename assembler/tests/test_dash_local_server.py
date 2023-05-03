0be524ee116dae1424e11a40e71d0271f2b660b5
tomrutherford <tmrutherford@gmail.com>
2023-05-03 14:18:10 -0700

    Extract dash

--- assembler/tests/test_dash_local_server.py
+++
@@ -1,9 +0,0 @@
-from assembler.renderers import dash_local_server
-
-
-def test_update():
-    client = dash_local_server.app.server.test_client()
-
-    client.post("/update", json={"test": 2})
-
-    assert 0
