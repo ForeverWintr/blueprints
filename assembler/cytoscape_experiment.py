d5a7558b2d85ee12d0531ee0b9e95ad808d3c8ad
tomrutherford <tmrutherford@gmail.com>
2023-05-03 14:19:20 -0700

    Revert "Extract dash"
    This reverts commit 0be524ee116dae1424e11a40e71d0271f2b660b5.

---
+++ assembler/cytoscape_experiment.py
@@ -0,0 +1,87 @@
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output, State
+import dash_cytoscape as cyto
+from dash import ctx
+
+from assembler.blueprint import Blueprint
+from assembler.tests.test_blueprint import Node
+from assembler.constants import (
+    NodeAttrs,
+    BuildStatus,
+    BUILD_STATUS_TO_COLOR,
+    MissingDependencyBehavior,
+)
+
+s = Node(name="source")
+a = Node(name="a", dependencies=(s,))
+d = Node(name="d")
+b = Node(name="out_b", dependencies=(a,))
+c = Node(name="out_c", dependencies=(a, d))
+
+bp = Blueprint.from_recipes([b, c])
+
+g = bp._dependency_graph
+elements = []
+for edge in g.edges:
+    a, b = (str(x) for x in edge)
+    for n in (a, b):
+        element = {
+            "data": {"id": n, "label": n},
+            "grabbable": False,
+            "classes": "square",
+        }
+        elements.append(element)
+    elements.append({"data": {"source": a, "target": b}})
+
+cyto.load_extra_layouts()
+app = Dash(__name__)
+
+
+cytoscape = cyto.Cytoscape(
+    id="cytoscape-two-nodes",
+    layout={"name": "dagre"},
+    style={"width": "100vw", "height": "90vh"},
+    elements=elements,
+    stylesheet=[  # Group selectors
+        {"selector": "node", "style": {"content": "data(label)"}},
+        # Class selectors
+        {"selector": ".red", "style": {"background-color": "red", "line-color": "red"}},
+        {"selector": ".square", "style": {"shape": "square"}},
+    ],
+)
+
+
+app.layout = html.Div(
+    [
+        cytoscape,
+        # The memory store reverts to the default on every page refresh
+        dcc.Store("step-index", data=1, storage_type="memory"),
+        html.Button("Previous", id="btn-prev"),
+        html.Button("Next", id="btn-next"),
+    ]
+)
+
+
+# @app.callback(
+# Output("cytoscape-elements-callbacks", "elements"),
+# Output("step-index", "data"),
+# Input("step-index", "data"),
+# Input("btn-prev", "n_clicks"),
+# Input("btn-next", "n_clicks"),
+# )
+def update_graph_scatter(step_idx, btn_prev_clicks, btn_next_clicks):
+    btn_clicked = ctx.triggered_id
+
+    if btn_clicked == "btn-next":
+        step_idx = step_idx + 1
+    elif btn_clicked == "btn-prev":
+        step_idx = step_idx - 1
+
+    while step_idx > len(steps):
+        steps.append(make_step())
+
+    return steps[step_idx - 1], step_idx
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ assembler/tests/test_dash_viz.py
@@ -0,0 +1,82 @@
+import plotly
+from plotly import graph_objects as go
+
+# from plotly import express as px # Requires pandas :|
+
+from assembler.blueprint import Blueprint, get_blueprint_layout
+from assembler.tests.test_blueprint import Node
+
+
+def test_visualize() -> None:
+    a = Node(name="a")
+    d = Node(name="d")
+    b = Node(name="b", dependencies=(a,))
+    c = Node(name="c", dependencies=(a, d))
+
+    bp = Blueprint.from_recipes([b, c])
+    layout = get_blueprint_layout(bp._dependency_graph)
+
+    edge_x = []
+    edge_y = []
+    node_x = []
+    node_y = []
+    seen = set()
+    for edge in bp._dependency_graph.edges():
+        for node in edge:
+            x, y = layout[node]
+            edge_x.append(x)
+            edge_y.append(y)
+            if not node in seen:
+                node_x.append(x)
+                node_y.append(y)
+                seen.add(node)
+
+        # Add None to break the line
+        edge_x.append(None)
+        edge_y.append(None)
+
+    edge_trace = go.Scatter(
+        x=edge_x,
+        y=edge_y,
+        line=dict(width=0.5, color="#888"),
+        hoverinfo="none",
+        mode="lines",
+    )
+
+    node_trace = go.Scatter(
+        x=node_x,
+        y=node_y,
+        mode="markers+text",
+        # hoverinfo="text",
+        marker_symbol="square",
+        marker_size=30,
+        text=[str(n) for n in bp._dependency_graph.nodes],
+    )
+    fig = go.Figure(
+        data=[edge_trace, node_trace],
+        layout=go.Layout(
+            title="<br>Network graph made with Python",
+            titlefont_size=16,
+            showlegend=False,
+            hovermode="closest",
+            margin=dict(b=20, l=5, r=5, t=40),
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        ),
+    )
+    # fig.show()
+    assert 0
+
+
+def test_cytoscape() -> None:
+    # https://dash.plotly.com/cytoscape
+    # SRSLY https://dash.plotly.com/cytoscape/callbacks
+    a = Node(name="a")
+    d = Node(name="d")
+    b = Node(name="b", dependencies=(a,))
+    c = Node(name="c", dependencies=(a, d))
+
+    bp = Blueprint.from_recipes([b, c])
+
+    g = bp._dependency_graph
+    assert 0
