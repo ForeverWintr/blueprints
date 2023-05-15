db0913f021d25e5f56d5c32c6f9110b91d17a0dd
Tom Rutherford <tmrutherford@gmail.com>
2023-05-15 15:47:28 -0700

    Fix db uri

--- assembler/renderers/dash_renderer/flask_app.py
+++ assembler/renderers/dash_renderer/flask_app.py
@@ -5,7 +5,12 @@ from assembler.renderers.dash_renderer.models import db
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+    # In memory db:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+
+    # For debugging db in viewer:
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/project.db"
 
     db.init_app(app)
     with app.app_context():
