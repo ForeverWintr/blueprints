3574a70a9c247697365b6dfa8cf82aaf47eaa7e4
tomrutherford <tmrutherford@gmail.com>
2023-05-15 19:04:51 -0600

    WIP. Is it a bad idea to use registry keys in db?

--- assembler/tests/test_dash_local_server.py
+++ assembler/tests/test_dash_local_server.py
@@ -7,6 +7,6 @@ def test_update(basic_blueprint: conftest.Blueprint):
     app = flask_app.create_app()
     client = app.test_client()
 
-    r = client.post("/blueprint", json=basic_blueprint.to_json())
+    r = client.post("/blueprint", json=basic_blueprint.to_serializable_dict())
 
     assert 0
