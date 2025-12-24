9391573c6281b2917ccafc0e472080011bdf8a06
tomrutherford <tmrutherford@gmail.com>
2025-12-24 12:45:45 -0700

    Disable some partially implemented stuff

--- blueprints/renderers/dash_local_server.py
+++ blueprints/renderers/dash_local_server.py
@@ -44,16 +44,16 @@ def update_graph_scatter(step_idx, btn_prev_clicks, btn_next_clicks):
     elif btn_clicked == "btn-prev":
         step_idx = step_idx - 1
 
-    while step_idx > len(steps):
-        steps.append(make_step())
+    # while step_idx > len(steps):
+    # steps.append(make_step())
 
-    return steps[step_idx - 1], step_idx
+    # return steps[step_idx - 1], step_idx
 
 
-@app.server.post("/update")
-def update():
-    """Update data, or shut down if we get a poison pill."""
-    asdf
+# @app.server.post("/update")
+# def update():
+# """Update data, or shut down if we get a poison pill."""
+# asdf
 
 
 if __name__ == "__main__":
